8ac621f3fa134c9c8043a33fea6923461bf8189b
marcinspython <stanilevitch@gmail.com>
2018-10-06 16:02:17 +0200

    Update zadanie_11

--- zjazd_1/zadanie_11.py
+++ zjazd_1/zadanie_11.py
@@ -7,10 +7,10 @@ pozycja_X = int(input("Podaj pozycję X "))
 pozycja_Y = int(input("Podaj pozycję Y "))
 
 
-prawy_górny_róg = pozycja_X >= 90 and pozycja_Y >= 90
-prawy_dolny_róg = pozycja_X >= 90 and pozycja_Y >= 10
-lewy_górny_róg = pozycja_X <= 10 and pozycja_Y >= 90
-lewy_dolny_róg = pozycja_X <= 10 and pozycja_Y <= 10
+# prawy_górny_róg = pozycja_X >= 90 and pozycja_Y >= 90
+# prawy_dolny_róg = pozycja_X >= 90 and pozycja_Y >= 10
+# lewy_górny_róg = pozycja_X <= 10 and pozycja_Y >= 90
+# lewy_dolny_róg = pozycja_X <= 10 and pozycja_Y <= 10
 
 if pozycja_X >= 90 and pozycja_Y >= 90:
     print("PGR")
