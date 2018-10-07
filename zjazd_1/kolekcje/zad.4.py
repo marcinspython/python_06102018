9d24bedb9b755cfe32e7f1326bb0c7b5314ab3da
marcinspython <stanilevitch@gmail.com>
2018-10-07 15:51:11 +0200

    zad.5 calculator

--- zjazd_1/kolekcje/zad.4.py
+++ zjazd_1/kolekcje/zad.4.py
@@ -1,4 +1,3 @@
-
 # wypisuje liczby od 0 do 100
 # for i in range(101):
 #     print(i)
@@ -6,14 +5,25 @@
 podzielna = 0
 # wypisuje liczby od 0 do 100 podzielne przez 3 lub podzielne przez 5
 for i in range(101):
-    if i%3==0 or i%5==0:
+    if i % 3 == 0 or i % 5 == 0:
         # print(i)
-        podzielna +=1
-
-
-print(podzielna)
-
-
+        podzielna += 1
 
 # napisz ile liczb wystąpiło w tym przedziale
 
+print(podzielna)
+
+print(f"W przedziale 0-100 liczb jest {podzielna} liczb podzielnych przez 3 i 5")
+
+
+
+
+# sposób rkorzen
+# wypisać
+lista = list(range(101))
+wynik = []
+for el in lista:
+    for el%3 == 0 or el%5 == 0:
+        wynik.append(el)
+
+print(len(wynik))
---
+++ zjazd_1/kolekcje/zad.5.py
@@ -0,0 +1,5 @@
+
+x=range(1, 500, 35) #przedział liczb z ilością znaków
+for i in x:
+    print(f'{i:7}', end='')
+print('costam')
