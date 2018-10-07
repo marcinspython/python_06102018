10fa7cd72166536ea5f20d4af735ae1695fd627c
marcinspython <stanilevitch@gmail.com>
2018-10-07 11:43:31 +0200

    zad.15 next2

--- zjazd_1/zadanie_15.py
+++ zjazd_1/zadanie_15.py
@@ -14,30 +14,45 @@ minimalna_liczba_krokow_po_wylosowaniu = abs(s_x - g_x) + abs(s_y - g_y)
 
 # zapisz w zmiennej
 
+# ustaw poczatkowa liczbe ruchów na 0
+liczba_ruchów = 0
 
+# mechanika
+while True:
+    min_1_ruch_przed = abs(s_x - g_x) + abs(s_y - g_y)
 
-# while True:
-# #     ruch = input("Popdaj liczbę lub wpisz KONIEC by zakonczyc")
-# #     if ruch.lower() == "koniec":
-# #         break
-# #
-# # # poruszanie gracza po planszy
-# # while True:
-# #
-# #     ruch = input()
-# #
-# # # w a s d
-# # if ruch == 'w':
-# #     pass
-# # if ruch == 'a':
-# #     pass
-# # if ruch == 's':
-# #     pass
-# # if ruch == 'd':
-# #     pass
+# poruszanie gracza po planszy
+while True:
+    min_1_ruch_przed = abs(s_x - g_x) + abs(s_y - g_y)
+    ruch = input("Wykonaj ruch [wasd]")
+    ruch = ruch.upper()
+
+# w a s d
+if ruch == 'w':
+    g_y += 1
+if ruch == 'a':
+    g_x -= 1
+if ruch == 's':
+    g_y -= 1
+if ruch == 'd':
+    g_x += 1
+# wyjście poza planszę
+if g_x > 10 or g_y > 10 or g_x < 1 or g_y < 1:
+    print("Wyszedłeś poza planszę")
 
 # oblicz minimalną liczbę kroków po ruchu
+min_1_ruch_po = abs(s_x - g_x) + abs(s_y - g_y)
 
+# sparawdz czy znalazł skarb
+if min_1_ruch_po == 0:
+    print("Wygrałeś")
+    # break
+
+# określ czy się przybliża czy oddala
+if min_1_ruch_po > min_1_ruch_przed:
+    print("Zimno")
+if min_1_ruch_po < min_1_ruch_przed:
+    print("Ciepło")
 
 print(f"Położenie skarbu: x={s_x}, y={s_y}")
 print(f"Położenie gracza: x={g_x}, y={g_y}")
