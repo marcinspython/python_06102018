868e57bf238c636d3b3dbf3d983d1bfbef0732d3
marcinstanilewicz <stanilewiczmarcin@gmail.com>
2018-10-10 01:33:51 +0200

    exrc ms_1

---
+++ zjazd_1/08-19.10.2018/ms_1.py
@@ -0,0 +1,20 @@
+a = 22.0
+b = 5.2
+
+sumaValue = a + b
+subtraction = a - b
+multi = a * b
+division0 = float(a / b)
+division1 = int(a / b)
+division2 = a // b
+division3 = a % b
+
+print(f"Suma: {sumaValue}")
+# assert sumaValue == 25
+
+print(f"Multi: {multi}")
+# assert multi == 100
+print(f"DIVISION_0: {division0}")
+print(f"DIVISION_1: {division1} float")
+print(f"DIVISION_2: {division2} int")
+print(f"DIVISION_3: {division3}")
