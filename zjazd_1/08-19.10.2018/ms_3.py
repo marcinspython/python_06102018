fd8820a65f02535e0b89b994ca726675b32966a4
marcinstanilewicz <stanilewiczmarcin@gmail.com>
2018-10-10 02:28:42 +0200

    exrc ms_3 ms_4

---
+++ zjazd_1/08-19.10.2018/ms_3.py
@@ -0,0 +1,13 @@
+wiek = 19
+
+print(f"Masz {wiek} lat, a za dwa lata bedziesz miec {wiek + 2} lat.")
+print(f"Czy jesteś pełnoletni?", wiek >= 18)
+
+czerokość = 20
+wysokość = 50
+
+wysokość = 2
+wysokość = 22
+pole = czerokość * wysokość
+
+print(f"Pole prostokąta: {pole}")
---
+++ zjazd_1/08-19.10.2018/ms_4.py
@@ -0,0 +1,9 @@
+a = 5
+if a < 10:
+    print(f"'a' jest mniejsze niż 10: {a} ")
+
+# #######################################################
+a = int(input(f"Podaj liczbę: "))
+while a < 10:
+    print(f"'a' jest mniejsze niż 10: {a} ")
+    a += 1
