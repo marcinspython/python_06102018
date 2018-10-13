f78521bb100fcdbaf9b2b0fdf36825a7fc085556
marcinstanilewicz <stanilewiczmarcin@gmail.com>
2018-10-14 00:55:05 +0200

    exercises on 'tuples'

--- snippets/tuple_krotki.py
+++ snippets/tuple_krotki.py
@@ -1,2 +1,21 @@
 (1, 2, 3)
+('abc', 1, 99.99)
 
+# ##### pusta tupla jedynie przez konstruktor #####
+tuple()
+
+# ##### operator dostępu // nawiasy kkwadratowe z indeksem w środku [] #####
+uczestnicy = ('Jan', 'Piotr', 'Ola', 'Tomasz', 'Sylwia')
+# uczestnicy[0]
+print(f'operator dostępu: {uczestnicy[0]}')
+print(f'operator dostępu: {uczestnicy[2]}')
+print(f'operator dostępu: {uczestnicy[4]}')
+# print(uczestnicy[11]) # IndexError:
+
+# ##### indeksowanie od tyłu kolekcji // ze znakiem "-" #####
+uczestnicy = ('Jan', 'Piotr', 'Ola', 'Tomasz', 'Sylwia')
+print(f'tupla uczestnicy = {uczestnicy}')
+uczestnicy[-1]
+print(f'indeksowanie od tyłu kolekcji dla -1: {uczestnicy[-1]}')
+print(f'indeksowanie od tyłu kolekcji dla -2: {uczestnicy[-2]}')
+print(f'indeksowanie od tyłu kolekcji dla -5: {uczestnicy[-5]}')
