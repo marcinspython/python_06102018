b8e0b279ea5839fe8b50f75799bf7f965d2f7af1
marcinstanilewicz <stanilewiczmarcin@gmail.com>
2018-10-15 00:29:00 +0200

    exercises tuple, sets, other methods

--- snippets/ms.py
+++ snippets/ms.py
@@ -1,58 +1,69 @@
-sprintNo = int(input("Podaj numer sprintu: "))
-print(sprintNo)
-
-# ######### pusty napis - konstruktor str()
-
-a = str()
-print(a)
-
-b = str(123)
-print(b)
-print(f"Wartość pusty napis:  {a} -tak, przed my slnikiem jest pusty napis")
-
-liczba_1 = 3
-liczba_2 = 9
-print(f"Wynik dodawania {liczba_1} + {liczba_2} = {liczba_1 + liczba_2}")
-
-# ######### bool()
-bool()
-bool(0)
-False
-bool(0.0)
-False
-bool(2)
-True
-bool('abc')
-
-# ######### zmienne
-wiek = 22
-print(str())
-print(f'Masz {wiek} lat, a za dwa lata będziesz mieć {wiek + 2} lat')
-print(f'Masz {wiek} lat, a za dwa lata będziesz mieć {wiek} + {2} lat')
-
-# ######### zmienne_2 śąćęś
-szerokosc = 10
-wysokość = 20
-pole_prostokata = wysokość * szerokosc
-
-print(pole_prostokata)
-print(f"Pole prostokąta wynosi: {pole_prostokata}")
+# sprintNo = int(input("Podaj numer sprintu: "))
+# print(sprintNo)
+#
+# # ######### pusty napis - konstruktor str()
+#
+# a = str()
+# print(a)
+#
+# b = str(123)
+# print(b)
+# print(f"Wartość pusty napis:  {a} -tak, przed my slnikiem jest pusty napis")
+#
+# liczba_1 = 3
+# liczba_2 = 9
+# print(f"Wynik dodawania {liczba_1} + {liczba_2} = {liczba_1 + liczba_2}")
+#
+# # ######### bool()
+# bool()
+# bool(0)
+# False
+# bool(0.0)
+# False
+# bool(2)
+# True
+# bool('abc')
+#
+# # ######### zmienne
+# wiek = 22
+# print(str())
+# print(f'Masz {wiek} lat, a za dwa lata będziesz mieć {wiek + 2} lat')
+# print(f'Masz {wiek} lat, a za dwa lata będziesz mieć {wiek} + {2} lat')
+#
+# # ######### zmienne_2 śąćęś
+# szerokosc = 10
+# wysokość = 20
+# pole_prostokata = wysokość * szerokosc
+#
+# print(pole_prostokata)
+# print(f"Pole prostokąta wynosi: {pole_prostokata}")
+# #########
+# aString = "Uwielbiam pisac w Pythonie"
+# print(f"Liczba znaków w zdaniu to: ", len(aString))
+# print(f"Trzeci znak to: ", aString[2])
+# print(f"Piąty znak to: ", aString[4])
+#
+# for znak in aString:
+#     print(znak)
+# #########
+#
+# aString = "Uwielbiam"
+# aString += " pisać"
+# print(aString + "w Pythonie")
+#
+# #########
+# aString = "Uwielbiam pisac w Pythonie"
+# print("5 pierwszych znaków to " + aString[:5])
+# print("A 8 ostatnich to: " + aString[-8:])
+# #########
+# x = "awesome"
+# print("Python is " + x)
 #########
-aString = "Uwielbiam pisac w Pythonie"
-print(f"Liczba znaków w zdaniu to: ", len(aString))
-print(f"Trzeci znak to: ", aString[2])
-print(f"Piąty znak to: ", aString[4])
-
-for znak in aString:
-    print(znak)
-#########
-
-aString = "Uwielbiam"
-aString += " pisać"
-print(aString + "w Pythonie")
+# x = 5
+x = str(5)
+y = "John"
+print(x + y)
 
 #########
-aString = "Uwielbiam pisac w Pythonie"
-print("5 pierwszych znaków to " + aString[:5])
-print("A 8 ostatnich to: " + aString[-8:])
-#########
+a = "Hello, World! "
+print(a.strip())
