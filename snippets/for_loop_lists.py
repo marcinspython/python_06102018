0752db3a5a7107fbbd1110b2b6b2a7c4a0b1e1bd
stanilevitch <stanilevitch@gmail.com>
2018-10-19 22:52:26 +0200

    For loop exercise

---
+++ snippets/for_loop_lists.py
@@ -0,0 +1,12 @@
+lista = ["a", "b", "c", "d", "e"]
+
+for litera in lista:
+    print(litera)
+# -------------------------------
+print("Second example")
+
+for litera in lista:
+    if litera == "c":
+        print(f"To jest litera 'c': = {litera}")
+    print(litera)
+# -------------------------------
