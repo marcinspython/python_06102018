834cb331be692fad606a231b9fc5a452e45b0b85
marcinspython <stanilevitch@gmail.com>
2018-10-20 10:36:04 +0200

    zad.9 słowniki

--- zjazd_2/zad_9.py
+++ zjazd_2/zad_9.py
@@ -1,11 +1,33 @@
+produkty = {"ziemniaki": 2, "bataty": 4, "pomidory": 6}
+magazyn = {"ziemniaki": 10, "bataty": 10, "pomidory": 10}
+do_zaplaty = 0
 
-ziemniaki = {'waga': 1, 'cena': 1.22}
-ziemniaki = {'waga': 1, 'cena': 1.22}
-ziemniaki = {'waga': 1, 'cena': 1.22}
-ziemniaki = {'waga': 1, 'cena': 1.22}
-ziemniaki = {'waga': 1, 'cena': 1.22}
+while True:  # 2 dodanie while
+    print("-" * 40)
+    print("Nasz zielnik oferuje: ")
+    for produkt in produkty.items():
+        name, cena = produkt
+        # cena = produkty[produkt]
+        # print(cena)
+        # print(f' - {produkt} - {produkty[produkt]} PLN') -to samo co poniżej
+        # print(f' - {produkt} - {cena} PLN')
+        print(f' - {name} - {cena} PLN')
 
-towar1 = {'nazwa': "ziemniaki", 'waga': 3}
-# towar2 = {'nazwa': "cebula", 'waga': 2}
 
-print(towar1)
+    komenda = input("Co chcesz zrobić: [k]upić, [koniec] by przerwać zakupy")  # 2
+    if komenda == 'koniec':  # 2
+        break
+    produkt_wybrany = input("Co chcesz kupić? ")
+    if produkt_wybrany not in produkty:
+        print("Nie mamy takiego produktu!")
+        continue
+
+    waga = float(input(f"Ile chcesz kupić wybranego produktu {[produkt_wybrany]}: "))
+
+    cena = produkty[produkt_wybrany]
+    koszt = waga * cena
+    do_zaplaty += koszt
+
+    print(f"Wyliczony koszt to: {koszt}")
+print("=" * 50)
+print(f"Za zakupy zaplacisz:  {do_zaplaty} PLN")
