23abe64b2acb0f418288da1f6732ecc32b572f21
marcinspython <stanilevitch@gmail.com>
2018-10-20 12:02:40 +0200

    Rozwiązanie zad.10 na 2 sposoby

--- zjazd_2/zad.10.py
+++ zjazd_2/zad.10.py
@@ -1,7 +1,18 @@
-
 napis = input("Podaj napis: ")
-
+liczniki_liter = {}
 # w podanym napisie zliczyć wystapienia poszczegolnych liter
 
+# zliczyć
+for litera in napis:
+    if litera in liczniki_liter:
+        liczniki_liter[litera] = liczniki_liter[litera] + 1
+    else:
+        liczniki_liter[litera] = 1
 
-print(napis)
+# zliczyć 2 razy
+for litera in napis:
+    liczniki_liter[litera] = liczniki_liter.get(litera, 0) + 1
+
+#wynik
+for litera in liczniki_liter:
+    print(f"litera: {litera} wystąpiła {liczniki_liter[litera]} razy")
