57efdfc14a9440f82b7e7099bc2752c82d2203ff
marcinspython <stanilevitch@gmail.com>
2018-10-20 15:42:09 +0200

    tests zad.1

---
+++ zjazd_2/funkcje/funkcje_example.py
@@ -0,0 +1,31 @@
+
+def przywitaj_sie():
+    return 1
+
+przywitaj_sie()
+print(przywitaj_sie)
+print(type(przywitaj_sie))
+print(type(przywitaj_sie()))
+
+print(przywitaj_sie())
+...imiona....
+
+# -----------------------------
+def czy_wieksza_niz_3(liczba)
+    if liczba>3:
+        return True
+    return False
+
+
+# -------- testy ---------------------
+def czy_wieksza_niz_3(liczba)
+    if liczba > 3:
+        return True
+    return False
+
+def test_wieksza_niz_3_dla_wiekszej_niz_3():
+    assert czy_wieksza_niz_3(4)
+
+def test_wieksza_niz_3_dla_mniejszej_niz_3():
+    assert czy_wieksza_niz_3(4)
+
