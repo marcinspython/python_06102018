39b419faed747965d4869af7109bed88596d8cd8
marcinspython <stanilevitch@gmail.com>
2018-11-03 16:41:10 +0100

    Zadanie3 w trakcie + Zadanie4

--- zjazd_3/Zadanie3.py
+++ zjazd_3/Zadanie3.py
@@ -1,15 +1,21 @@
+class ElectricCar:
+    def __init__(self, max_range):
+        self.max_range = max_range
+        self.possible_distance = max_range
 
+    def drive(self, distance):
+        self.possible_distance -= distance
 
-car = ElectricCar(100)
-car.drive(70) == 70
-70
-car.drive(50)
-30
-car.drive(50)
-0
-car.charge()
-car.drive(50)
-50
+        return distance
 
 
 
+def test_electric_car():
+    car = ElectricCar(100)
+    assert car.drive(70) == 70
+    assert car.drive(50) == 30
+    assert car.drive(50) == 0
+    car.charge()
+    assert car.drive(50) == 50
+    car.charge()
+    assert car.drive(120) == 100
