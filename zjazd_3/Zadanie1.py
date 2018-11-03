13c336d44d05780b5909e9baa40edc0cb73ed594
marcinspython <stanilevitch@gmail.com>
2018-11-03 10:53:49 +0100

    Klasy - Zadanie1 - rozwiazanie

--- zjazd_3/Zadanie1.py
+++ zjazd_3/Zadanie1.py
@@ -9,15 +9,22 @@ Produkt "Woda", id: 1, cena: 10.99 PLN
 """
 
 
-class Product(object, ID, cena):
-    pass
+class Product(object):
+    def __init__(self, id, nazwa, cena):
+        self.ID = id
+        self.nazwa = nazwa
+        self.cena = cena
+
+    def print_info(self):
+        return f'Produkt "{self.nazwa}", id: {self.ID}, cena: {self.cena} PLN'
 
 
 def test_product():
     product = Product(1, 'Woda', 10.99)
     assert product.ID == 1
-    assert product.nazwa == 'Woda'
+    assert product.nazwa == "Woda"
     assert product.cena == 10.99
+
 
 def test_product_print():
     product = Product(1, 'Woda', 10.99)
