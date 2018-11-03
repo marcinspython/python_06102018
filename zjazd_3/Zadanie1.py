5b9d70f19495ed4c3cd423f6da8c00a45d2bfac5
marcinspython <stanilevitch@gmail.com>
2018-11-03 10:38:04 +0100

    Klasy - Zadanie1

--- zjazd_3/Zadanie1.py
+++ zjazd_3/Zadanie1.py
@@ -1,3 +1,16 @@
+"""
+Zaimplementuj klase Product przechowujaca informacje o cenie,
+nazwie oraz ID produktu. Zaimplementuj metode wypisujaca
+informacje o produkcie na konsole.
+Przykład uzycia:
+>>> product = Product(1, 'Woda', 10.99)
+>>> product.print_info()
+Produkt "Woda", id: 1, cena: 10.99 PLN
+"""
+
+
+class Product(object, ID, cena):
+    pass
 
 
 def test_product():
