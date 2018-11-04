6ec754b1da980314aeecac09da0d720ad0dba39c
marcinspython <stanilevitch@gmail.com>
2018-11-04 16:14:25 +0100

    Operacje na plikach files_zad2.py - POPRAWKA

--- zjazd_3/files_zad2.py
+++ zjazd_3/files_zad2.py
@@ -15,10 +15,12 @@ with open(file_name) as f:
         if action == "LOGIN":
             user_last_login[login] = time
         if action == "LOGOUT":
-            user_total_time[login] = user_total_time[login, 0] + time - user_last_login[login]
+            user_total_time[login] = user_total_time.get(login, 0) + time-user_last_login[login]
+
 
 def sort_key(x):
     return x[1]
+
 
 print("Czas przebywania w systemie: ")
 for item in sorted(user_total_time.items(), key=lambda x: x[1], reverse=True):
