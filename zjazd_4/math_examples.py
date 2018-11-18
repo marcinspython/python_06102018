3fde6926ca2f1b3590058eeb2e5e8169b6a1bb24
marcinspython <stanilevitch@gmail.com>
2018-11-18 16:57:22 +0100

    random

--- zjazd_4/math_examples.py
+++ zjazd_4/math_examples.py
@@ -1,7 +1,32 @@
 import math
 
-print(math.sin(math.pi/2))
+print(math.sin(math.pi / 2))
 
 print(dir(math))
 print(math.degrees(90))
 print(math.tau)
+
+"""
+Stwórz klasę sfera:
+s = Sfera(10)
+s.promien # 10
+s.objetosc() # 4188.78...
+s.pole_powierzchni() # 1256.63...
+
+"""
+
+class Kula:
+
+    def __init__(self, r):
+        self.promien = r
+
+    def objetosc(self):
+        return (4/3) * math.pi * math.pow(self.promien, 3)
+
+    def pole_poweirzchni(self):
+        return 4 * math.pi * self.promien ** 2
+
+s = Kula(10)
+
+print(s.objetosc())
+print(s.pole_poweirzchni())
---
+++ zjazd_4/random_examples.py
@@ -0,0 +1,5 @@
+import random
+
+random.seed(3) #stała warotość
+print(random.random())
+
